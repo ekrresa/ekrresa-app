58bb8c9d84797ae6a9a1748733ed1d05820b275b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2026-08-08 23:26:22 +0100

    feat: set up site favicon

--- src/app/document.tsx
+++ src/app/document.tsx
@@ -11,6 +11,12 @@ export function Document({ children, ctx }: DocumentProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="color-scheme" content="light dark" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
         <title>{siteMetadata.title}</title>
         <meta name="description" content={siteMetadata.description} />
         <link rel="canonical" href={siteMetadata.siteUrl} />
